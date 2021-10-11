a4982e86abd31f84b9604894a0d096ede5f2dd91
AhmedHallag <aalhallag@gmail.com>
2021-10-11 05:01:04 +0200

    Added a different searching algo

--- search_algos.py
+++ search_algos.py
@@ -1,3 +1,7 @@
 # Linear search algorithim will implemented here..
 
 print('*** Linear Search implemented. *** ')
+
+# Sub-String Matching Algorithim
+
+print('*** Sub-String Search implemented. *** ')
